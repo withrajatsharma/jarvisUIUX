fa33210fedc58f649120656876b085facb035b8c
RajatSharma <withrajatsharma@gmail.com>
2024-08-28 11:46:22 +0530

    mobile chats

--- Mobile ui/script.js
+++ Mobile ui/script.js
@@ -1,22 +1,33 @@
-const chats  = document.querySelector('.chats');
+const chats  = document.querySelector('.chat');
 
-const input = document.querySelector('#askAi');
+const input = document.querySelector('input');
 
-const btn = document.querySelector('button');
+const btn = document.querySelector('.send-button');
 
 
 
 
 btn.onclick = () =>{
 
+
     if(input.value==""){
         return;
     }else{
-        const p = document.createElement('p');
-        p.innerHTML = `<span id="user">Kaushik: </span> ${input.value}`;
-        chats.appendChild(p);
+        // const p = document.createElement('p');
+        const div = document.createElement('div');
+        // const divicon = document.createElement('div');
+        div.className='message user';
+
+        div.innerHTML = ` <p class="p1">${input.value}</p>
+               
+                <div class="icon user-icon">&#x1F464;</div>`;
+
+
+        // p.innerHTML = `<span id="user">Kaushik: </span> ${input.value}`;
+        chats.appendChild(div);
         input.value = "";
     }
+
   
 
 }
