810deff0bda869e5414974d95f44a73c0e37473c
RajatSharma <withrajatsharma@gmail.com>
2024-08-28 11:47:37 +0530

    Update script.js

--- Mobile ui/script.js
+++ Mobile ui/script.js
@@ -13,9 +13,7 @@ btn.onclick = () =>{
     if(input.value==""){
         return;
     }else{
-        // const p = document.createElement('p');
         const div = document.createElement('div');
-        // const divicon = document.createElement('div');
         div.className='message user';
 
         div.innerHTML = ` <p class="p1">${input.value}</p>
@@ -23,7 +21,6 @@ btn.onclick = () =>{
                 <div class="icon user-icon">&#x1F464;</div>`;
 
 
-        // p.innerHTML = `<span id="user">Kaushik: </span> ${input.value}`;
         chats.appendChild(div);
         input.value = "";
     }
